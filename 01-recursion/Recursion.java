99451ee93e1e67c6e3b3a12b6cf712eddc053cac
Nurdaulet Kaishibayev <nkaishibayev@gmail.com>
2022-02-03 14:34:18 -0500

    Recursion lab work for February 3 finished: converted the boolean into an if else if statements in the for loop thaht would check for next letter and length of the word

--- 01-recursion/Recursion.java
+++ 01-recursion/Recursion.java
@@ -20,7 +20,42 @@ public class Recursion {
     }
   }
 
+  /*Print all words that are made of the characters in the array of letters.
+  *There may not be consecutive equal letters, so:
+  *aax is not allowed, but axa is allowed.
+  *@param length : the length of the words that are to be printed
+  *@param letters: the letters you should be using,
+  *@precondition: letters contains at least 2 characters, and has no duplicates.
+  */
+  public static void printNoDoubleLetterWords(int length,char[] letters){
+    printNoDoubleLetterWords(length,"",letters);
+  }
+
+  /*Print all words that are made of the characters in letters. There may not be consecutive equal letters,
+  *aax is not allowed, but axa is allowed.
+  *@param length : either how many more letters need to be
+  *@param word   : the partial word so far.
+  *@param letters: the letters you should be using
+  */
+  public static void printNoDoubleLetterWords(int length,String word,char[]letters){
+    if (length == 0) {
+      System.out.println(word);
+    } else {
+      int size = letters.length;
+      for (char c = 0; c < size; c++) {
+        if (word.length() == 0) {
+          printNoDoubleLetterWords(length-1, word + letters[c], letters);
+        } else if (word.charAt(word.length()-1) != letters[c]) {
+          printNoDoubleLetterWords(length-1, word + letters[c], letters);
+        }
+      }
+    }
+  }
+
+
   public static void main(String[] args) {
-    printAllWords(2, "");
+    //printAllWords(2);
+    //char[] chars = {'a', 'b', 'z'};
+    //printNoDoubleLetterWords(3, chars);
   }
 }
