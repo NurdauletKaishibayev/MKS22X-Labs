f08252522ea18184683ea96d9e7a685b13594f90
Nurdaulet Kaishibayev <nkaishibayev@gmail.com>
2022-02-09 11:13:33 -0500

    RecursionClasswork: Pasted and edited the current code from PartialSum

--- classwork/RecursionClasswork.java
+++ classwork/RecursionClasswork.java
@@ -1,3 +1,23 @@
 public class RecursionClasswork{
-  
+  // Group Sum
+  public static boolean groupSum(int start, int[] nums, int target) {
+    if (target == 0) {
+      return true;
+    }
+    if (start == nums.length - 1) {
+      return false;
+    } else {
+      if (nums[start+1] > target) {
+        return groupSum(start + 1, nums, target);
+      } else {
+        return groupSum(start+1, nums, target) ||
+        groupSum(start+1, nums, target - nums[start+1]);
+      }
+    }
+  }
+
+  public static void main(String[] args) {
+    int[] arr1 = {2,4,8};
+    System.out.println(groupSum(0,arr1,10));
+  }
 }
