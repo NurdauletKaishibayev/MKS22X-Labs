1cd917a5a4271bb04142b9ba73cb71db7a85af46
Nurdaulet Kaishibayev <nkaishibayev@gmail.com>
2022-02-14 11:26:59 -0500

    Coded toString method

--- 02-queens/QueenBoard.java
+++ 02-queens/QueenBoard.java
@@ -8,6 +8,7 @@ public class QueenBoard {
         board[i][j] = 0;
       }
     }
+  }
    /**
    *@return The output string formatted as follows:
    *All numbers that represent queens are replaced with 'Q'
@@ -21,8 +22,19 @@ public class QueenBoard {
    */
    public String toString(){
      int size = board.length;
-     
-   }
+     String output = "";
+     for (int i = 0; i < size; i++) {
+       for (int j = 0; j < size; j++) {
+         if (board[i][j] == -1) {
+           output += board[i][j] + " ";
+         } else {
+           output += "_" + " ";
+         }
+       }
+       output += "\n";
+     }
+     return output;
+    }
 
    /**
    *@return true when the queen added correctly, false Otherwise
@@ -30,18 +42,18 @@ public class QueenBoard {
    *@postcondition the board is only changed when the function returns true
    * in which case the queen is added and all it's threatened positions are incremented
    */
-   private boolean addQueen(int r, int c){
-
-   }
+   // private boolean addQueen(int r, int c){
+   //
+   // }
 
    /**Remove the queen that was added to r,c
    *@precondition r and c are valid indices of the board array and there is a queen at position r,c
    *@postcondition the board is modified to remove that queen and all it's
    *threatened positions are decremented
    */
-   private void removeQueen(int r, int c){
-
-   }
+   // private void removeQueen(int r, int c){
+   //
+   // }
 
    /**Find the first solution configuration possible for this size board. Start by placing
    *  the 1st queen in the top left corner, and each new queen in the next ROW. When backtracking
@@ -52,16 +64,16 @@ public class QueenBoard {
    *        returns true when the board is solveable, and leaves the board in a solved state
    *@throws IllegalStateException when the board starts with any non-zero value (e.g. you solved a 2nd time.)
    */
-   public boolean solve(){
-
-   }
+   // public boolean solve(){
+   //
+   // }
 
    /**Find all possible solutions to this size board.
    *@return the number of solutions found, and leaves the board filled with only 0's
    *@throws IllegalStateException when the board starts with any non-zero value (e.g. you ran solve() before this method)
    */
-   public int countSolutions(){
-
-   }
+   // public int countSolutions(){
+   //
+   // }
 
 }
