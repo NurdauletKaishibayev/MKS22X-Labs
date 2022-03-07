5f029edb6672b9023f835c5e6c42d42e6620d15c
Nurdaulet Kaishibayev <nkaishibayev@gmail.com>
2022-03-07 02:48:46 -0500

    Made a wrapper generator method which will place the S and E at the end

--- 03-maze/MazeGenerator.java
+++ 03-maze/MazeGenerator.java
@@ -1,40 +1,59 @@
-public class MazeGenerator {
+import java.util.*;
 
+public class MazeGenerator {
 
   public static boolean safe(char[][] maze, int startrow, int startcol) {
     int counter = 0;
-    if (maze[startrow+1][startcol] == ' ') {
+    int rowLen = maze.length;
+    int colLen = maze[0].length;
+
+    if (startrow == rowLen-1 || startcol == 0 || startrow == 0 || startcol == colLen -1) {
+      return false;
+    }
+
+    if ((startrow + 1 < rowLen-1) && maze[startrow+1][startcol] == ' ') {
       counter++;
     }
-    if (maze[startrow][startcol+1] == ' ') {
+    if ((startcol + 1 < colLen-1) && maze[startrow][startcol+1] == ' ') {
       counter++;
     }
-    if (maze[startrow-1][startcol] == ' ') {
+    if ((startrow - 1 > 0) && maze[startrow-1][startcol] == ' ') {
       counter++;
     }
-    if (maze[startrow][startcol-1] == ' ') {
+    if ((startcol - 1 > 0) && maze[startrow][startcol-1] == ' ') {
       counter++;
     }
-    if (counter < 2) {
-      return true;
-    }
-    return false;
+    return (counter < 2);
   }
 
-
+  // Wrapper method
   public static void generate(char[][]maze,int startrow,int startcol) {
+    generate(maze, startrow, startcol, 1);
+    maze[startrow][startcol] = 'S';
+  }
+
+  public static void generate(char[][]maze, int startrow, int startcol, int regular) {
     int rowLen = maze.length;
     int colLen = maze[0].length;
     int counter = 0;
 
     maze[startrow][startcol] = ' ';
-    int x = (int)(Math.random() * (3) + 0)-1;
-    int y = (int)(Math.random() * (3) + 0)-1;
 
-    if ((startrow + x > 0 && startrow + x < rowLen-1) && (startcol + y > 0 && startcol + y < colLen-1)) {
-      if (safe(maze, startrow+x, startcol+y)) {
-        generate(maze, startrow + x, startcol + y);
-      }
+    ArrayList<int[]> coords = new ArrayList<int[]>();
+    int[] up = {0, -1};
+    int[] down = {0, 1};
+    int[] left = {-1, 0};
+    int[] right = {1,0};
+    coords.add(up);
+    coords.add(down);
+    coords.add(left);
+    coords.add(right);
+    Collections.shuffle(coords);
+    int x = coords.get(0)[0];
+    int y = coords.get(0)[1];
+    if (safe(maze, startrow+x, startcol+y)) {
+      coords.remove(0);
+      generate(maze, startrow+x, startcol+y, 1);
     }
   }
 
