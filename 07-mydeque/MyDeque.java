22ba24b5684dcf0c7defc3ea83e916a292fc4f13
Nurdaulet Kaishibayev <nkaishibayev@gmail.com>
2022-03-28 14:27:48 -0400

    Coded and briefly tested the toString and toStringDebug functions

--- 07-mydeque/MyDeque.java
+++ 07-mydeque/MyDeque.java
@@ -6,23 +6,75 @@ public class MyDeque<E>{
   private int start, end;
 
   public MyDeque(){
-    @SuppressWarnings("unchecked")
-    E[] d = (E[])new Object[10];
-    data = d;
+    // Calls the bottom constructor which creates a size 10 array
+    this(10);
   }
 
   public MyDeque(int initialCapacity){
     @SuppressWarnings("unchecked")
     E[] d = (E[])new Object[initialCapacity];
     data = d;
+    start = data.length/2;
+    end = data.length/2;
+    size = 0;
+  }
+
+  // call when start = length
+  private void resize() {
+
   }
 
   public int size(){
-
+    return size;
   }
 
+  // toString that prints values from start to end in ORDER
   public String toString(){
+    String out = "[";
+    if (end < start) {
+      for (int i = start; i < data.length; i++) {
+        out += data[i] + ", ";
+      }
+      for (int j = 0; j <= end; j++) {
+        if (j == end) {
+          out += data[j] + "]";
+        } else {
+          out += data[j] + ", ";
+        }
+      }
+      return out;
+    } else {
+      for (int i = start; i <= end; i++) {
+        if (i == end) {
+          out += data[i] + "]";
+        } else {
+          out += data[i] + ", ";
+        }
+      }
+      return out;
+    }
+  }
 
+
+  // Prints out the entire array with all nulls and etc.
+  public String toStringDebug() {
+    String out = "[";
+    for (int i = 0; i < data.length; i++) {
+      if (i == data.length-1) {
+        if (data[i] == null) {
+          out += "null]";
+        } else {
+          out += data[i] + "]";
+        }
+      } else {
+        if (data[i] == null) {
+          out += "null, ";
+        } else {
+          out += data[i] + "]";
+        }
+      }
+    }
+    return out;
   }
 
   public void addFirst(E element) throws NullPointerException{
@@ -33,19 +85,36 @@ public class MyDeque<E>{
 
   }
 
-  public E removeFirst() throws NoSuchElementException{
+  // public E removeFirst() {
+  //   if (size == 0) {
+  //     throw new NoSuchElementException();
+  //   }
+  //
+  // }
+  //
+  // public E removeLast() {
+  //   if (size == 0) {
+  //     throw new NoSuchElementException();
+  //   }
+  // }
 
+  public E getFirst() {
+    if (size == 0) {
+      throw new NoSuchElementException();
+    }
+    return data[start];
   }
 
-  public E removeLast() throws NoSuchElementException{
-
+  public E getLast() {
+    if (size == 0) {
+      throw new NoSuchElementException();
+    }
+    return data[end];
   }
 
-  public E getFirst() throws NoSuchElementException{
-
-  }
-
-  public E getLast() throws NoSuchElementException{
-
+  public static void main(String[] args) {
+    MyDeque<Integer> deque = new MyDeque<Integer>();
+    System.out.println(deque);
+    System.out.println(deque.toStringDebug());
   }
 }
