199cebaf260caab27d91c59e47e15126a7fdf2e6
Nurdaulet Kaishibayev <nkaishibayev@gmail.com>
2022-04-03 23:34:34 -0400

    FINISHED resize and add/remove functions

--- 07-mydeque/MyDeque.java
+++ 07-mydeque/MyDeque.java
@@ -21,7 +21,45 @@ public class MyDeque<E>{
 
   // call when size = length of the array
   private void resize() {
-    data = Arrays.copyOf(data, (data.length * 2)+1 );
+    @SuppressWarnings("unchecked")
+    E[] newArr = (E[])new Object[data.length * 2 + 1];  // Make array length of double that of data (+1 for the 0 length case)
+    if (start == 0) {
+      int count = 0;
+      for (int i = start; i < data.length; i++) {
+        newArr[count] = data[i];
+        count++;
+      }
+      end = count;
+
+
+    } else {
+
+
+      int count = 0;
+      for (int i = start; i < data.length; i++) {
+        newArr[count] = data[i];
+        count++;
+      }
+
+      for (int j = 0; j < end; j++) {
+        newArr[count] = data[j];
+        count++;
+      }
+      start = 0;
+      end = count;
+    }
+
+    // else {
+    //   int count = 0;
+    //   for (int i = start; i < end; i++) {
+    //     newArr[count] = newArr[i];
+    //     count++;
+    //   }
+    //   start = 0;
+    //   end = count-1;
+    // }
+
+    data = newArr;
   }
 
   public int size(){
@@ -30,22 +68,55 @@ public class MyDeque<E>{
 
   // toString that prints values from start to end in ORDER
   public String toString(){
-    String out = "[";
-    if (end < start) {
-      for (int i = start+1; i < data.length; i++) {
+     if (size() == 0) {
+       return "[]";
+     }
+
+       String out = "[";
+       if (end < start) {
+         for (int i = start; i < data.length; i++) {
+           if (end == 0) {
+             if (i == data.length-1) {
+               out += data[i];
+               break;
+             } else {
+               out += data[i] + ", ";
+             }
+           } else {
+             out += data[i] + ", ";
+           }
+
+
+         }
+         for (int j = 0; j < end; j++) {
+           if (j == end-1) {
+             out += data[j];
+           } else {
+             out += data[j] + ", ";
+           }
+         }
+         return out + "]";
+
+
+
+
+
+    } else if (start == end) {
+      for (int i = start; i < data.length; i++) {
         out += data[i] + ", ";
       }
-      for (int j = 0; j <= end; j++) {
-        if (j == end) {
+      for (int j = 0; j < end; j++) {
+        if (j == end-1) {
           out += data[j];
         } else {
           out += data[j] + ", ";
         }
       }
       return out + "]";
+
     } else {
-      for (int i = start+1; i <= end; i++) {
-        if (i == end) {
+      for (int i = start; i < end; i++) {
+        if (i == end-1) {
           out += data[i];
         } else {
           out += data[i] + ", ";
@@ -91,9 +162,16 @@ public class MyDeque<E>{
     if (size() == data.length) {
       resize();
     }
-    data[start] = element;
-    size++;
-    start--;
+
+    if (start == 0) {
+      start = data.length-1;
+      data[start] = element;
+      size++;
+    } else {
+      start--;
+      data[start] = element;
+      size++;
+    }
   }
 
   public void addLast(E element) {
@@ -111,60 +189,113 @@ public class MyDeque<E>{
     }
 
     if (end == data.length-1) {
-      end = 0;
       data[end] = element;
+      end = 0;
       size++;
     } else {
-      data[end+1] = element;
+      data[end] = element;
       size++;
       end++;
     }
   }
 
-  // public E removeFirst() {
-  //   if (size == 0) {
-  //     throw new NoSuchElementException("Array is Empty!");
-  //   }
-  //
-  // }
-  //
-  // public E removeLast() {
-  //   if (size == 0) {
-  //     throw new NoSuchElementException("Array is Empty!");
-  //   }
-  // }
+  public E removeFirst() {
+    if (size == 0) {
+      throw new NoSuchElementException("Array is Empty!");
+    }
+    E elm = getFirst();
+    data[start] = null;
+    start++;
+    size--;
+    return elm;
+  }
+
+  public E removeLast() {
+    if (size == 0) {
+      throw new NoSuchElementException("Array is Empty!");
+    }
+    E elm = getLast();
+    if (end == 0) {
+      end = data.length-1;
+      data[end] = null;
+      end--;
+      size--;
+      return elm;
+    } else {
+      data[end-1] = null;
+      end--;
+      size--;
+      return elm;
+    }
+  }
 
   public E getFirst() {
     if (size() == 0) {
       throw new NoSuchElementException("Array is Empty!");
     }
-    return data[start+1];
+    return data[start];
   }
 
   public E getLast() {
     if (size() == 0) {
       throw new NoSuchElementException("Array is Empty!");
     }
-    return data[end];
+    if (end == 0) {
+      return data[data.length-1];
+    } else {
+      return data[end-1];
+    }
   }
 
-  public static void main(String[] args) {
-    MyDeque<Integer> deque = new MyDeque<Integer>();
-    deque.addFirst(2);
-    deque.addFirst(3);
-    deque.addFirst(4);
-    deque.addFirst(5);
-    deque.addLast(10);
-    deque.addLast(11);
-    deque.addLast(12);
-    deque.addLast(13);
-    deque.addLast(14);
-    deque.addFirst(15);
-
-    System.out.println(deque.getFirst());
-    System.out.println(deque.getLast());
-    System.out.println(deque.size());
-    System.out.println(deque);
-    System.out.println(deque.toStringDebug());
+  public void start() {
+    System.out.println(start);
   }
+
+  public void end() {
+    System.out.println(end);
+  }
+
+  // public static void main(String[] args) {
+  //   MyDeque<Integer> deque = new MyDeque<Integer>();
+  //   deque.addFirst(2);
+  //   deque.addFirst(3);
+  //   // // deque.addFirst(4);
+  //   // // deque.addFirst(5);
+  //   // // deque.addFirst(6);
+  //   // // deque.addFirst(0101);
+  //   // // deque.addFirst(0);
+  //   // // deque.addLast(888);
+  //   // // deque.addLast(11);
+  //   deque.addLast(1);
+  //   deque.addLast(4);
+  //   deque.addLast(5);
+  //   deque.addLast(6);
+  //   deque.addLast(7);
+  //   deque.addLast(8);
+  //   deque.addLast(9);
+  //   // deque.addLast(10);
+  //   // deque.addLast(12);
+  //   // deque.addLast(15);
+  //   // deque.addFirst(16);
+  //   // deque.removeLast();
+  //   // deque.removeLast();
+  //   // deque.removeLast();
+  //   // deque.removeFirst();
+  //
+  //
+  //
+  //
+  //
+  //
+  //   System.out.println(deque.getFirst());
+  //   System.out.println(deque.getLast());
+  //   System.out.println(deque.size());
+  //   System.out.println(deque);
+  //   System.out.println(deque.toStringDebug());
+  //
+  //   System.out.println("-=-=-=-= START =-=-=-=-");
+  //   deque.start();
+  //   System.out.println("-=-=-=-= END =-=-=-=-");
+  //   deque.end();
+  // }
 }
